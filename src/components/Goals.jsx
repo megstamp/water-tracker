1867a78cd2599963a43b33e6e3f948d1c2ffbcda
Meghann Stamper <meg.stamp@yahoo.com>
2023-05-11 17:12:43 -0400

    swipe up and down

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -1,13 +1,11 @@
-import { Box, InfoIcon, ArrowUpIcon, ArrowDownIcon, Text } from "native-base"
+import { Box, InfoIcon, Text } from "native-base"
 
-export default function Goals({ goal, setGoal }){
+export default function Goals({ goal }){
     return (
         <Box height={100} width="100%" alignItems="center" justifyContent="center">
             <Text color="blue.300" fontSize="lg">
-                <ArrowUpIcon color="blue.100" onPress={() => setGoal(goal + 500)} />{""}
                 <InfoIcon color="blue.300" mr={2} />
                 Water Target: {goal} ml
-                <ArrowDownIcon color="blue.100" onPress={() => setGoal(goal - 500)} />{""}
             </Text>
         </Box>
     )
